b5d87444294eddb845f50f86d86e10f9a4bc106d
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2022-11-20 16:33:23 +0100

    Add auth

--- smartCardProject/SmartCardProject.java
+++ smartCardProject/SmartCardProject.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package smartCardProject;
 
 import javacard.framework.Applet;
@@ -10,18 +7,49 @@ import javacard.framework.APDU;
 import javacard.framework.Util;
 import javacard.security.KeyPair;
 import javacard.security.KeyBuilder;
+import javacard.framework.OwnerPIN;
 
+// use OwnerPIN for PIN code ?
 
-public class SmartCardProject extends Applet {
+public class SmartCardProject extends Applet
+{
+	/// Instructions list
+	// Convention: use same digit for linked instructions
+	// ie: 0x2n -> login, logout, change pin...
+	private static final byte INST_HELLO = 0x10;
+	private static final byte INST_AUTH = 0x20;
 	
-	private final static byte[] hello=
+	
+	public static final short DEFAULT_PIN_CODE = 0000;
+	private static final short PIN_MAX_RETRIES = 5;
+	private static final short PIN_SIZE = 4;
+	
+	private final static byte[] HELLO_STR =
 	{0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x72, 0x6f, 0x62, 0x65, 0x72, 0x74};
+	
+	private final static byte[] OK_RESPONSE = {'O', 'K'};
+	private final static byte[] KO_RESPONSE = {'K', 'O'};
 	
 	private javacard.security.PublicKey publicRSAKey = null;
 	private javacard.security.PrivateKey privateRSAKey = null;
+	private short pinCode = DEFAULT_PIN_CODE;
+	private boolean cardConnected = false;
 	
-	public static void install(byte[] buffer, short offset, byte length) 
+	private OwnerPIN ownerPin;
 	
+	private class APDUData
+	{
+		public APDU apdu;
+		public byte[] apduBuffer;
+		
+		public APDUData(APDU apdu, byte[] apduBuffer)
+		{
+			this.apdu = apdu;
+			this.apduBuffer = apduBuffer;
+		}
+	}
+	
+	public static void install(byte[] buffer, short offset, byte length)
 	{
 		// GP-compliant JavaCard applet registration
 		SmartCardProject smartCardProject = new SmartCardProject();
@@ -33,24 +61,96 @@ public class SmartCardProject extends Applet {
 		
 	}
 	
+	SmartCardProject()
+	{
+		ownerPin = new OwnerPIN((byte)PIN_MAX_RETRIES, (byte)PIN_SIZE);
+	}
+	
 	// TODO: use https://github.com/neonlzf/smartcard/blob/master/Code/OnCard/DES/RSA.java
 
 	public void process(APDU apdu) {
 		// Good practice: Return 9000 on SELECT
 		if (selectingApplet()) {
-			return;
+			ISOException.throwIt(ISO7816.SW_NO_ERROR);
 		}
 
-		byte[] buf = apdu.getBuffer();
-		switch (buf[ISO7816.OFFSET_INS]) {
-		case (byte) 0x40:
-			Util.arrayCopy(hello, (byte)0, buf, ISO7816.OFFSET_CDATA, (byte)12);
-				apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (byte)12);
+		byte[] apduBuffer = apdu.getBuffer();
 		
+		// SELECT
+		if ((apduBuffer[ISO7816.OFFSET_CLA] == 0) && (apduBuffer[ISO7816.OFFSET_INS] == (byte) 0xA4)) {
+            		return;
+        	}
+
+        	if (apduBuffer[ISO7816.OFFSET_CLA] != 0x0) {
+            		ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+        	}
+		
+		// Retrieve command data
+		/*short bytesLeft = Util.makeShort((byte) 0x00, apduBuffer[ISO7816.OFFSET_LC]);
+        	if (bytesLeft != apdu.setIncomingAndReceive()) {
+            		ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+        	}*/
+		
+		switch (apduBuffer[ISO7816.OFFSET_INS]) {
+		case INST_HELLO:
+			instHello(apdu);
+			break;
+		case INST_AUTH:
+			instAuth(apdu);
 			break;
 		default:
 			// good practice: If you don't know the INStruction, say so:
 			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
 		}
+	}
+	
+	/// Instructions methods
+	
+	private void instHello(APDU apdu)
+	{
+		sendAPDUResponse(apdu, HELLO_STR);
+	}
+	
+	private void instAuth(APDU apdu) // TODO: error message more clear
+	{
+		if (ownerPin.isValidated())
+		{
+			sendAPDUResponse(apdu, OK_RESPONSE);
+			return;
+		}
+		if (ownerPin.getTriesRemaining() == 0)
+		{
+			sendAPDUResponse(apdu, KO_RESPONSE);
+			return;
+		}
+		
+		short bytesLeft = apdu.getBuffer()[ISO7816.OFFSET_LC];
+        	if (bytesLeft != apdu.setIncomingAndReceive()) {
+            		ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+        	}
+		
+		byte[] apduBuffer = apdu.getBuffer();
+		
+		if (apduBuffer[ISO7816.OFFSET_LC] != PIN_SIZE) {
+            		sendAPDUResponse(apdu, KO_RESPONSE);
+			return;
+        	}
+        	
+        	
+        	// Code is correct
+        	if (ownerPin.check(apduBuffer, ISO7816.OFFSET_CDATA, (byte)PIN_SIZE))
+        	{
+        		sendAPDUResponse(apdu, OK_RESPONSE);
+			return;
+        	}
+        	
+		sendAPDUResponse(apdu, KO_RESPONSE);
+	}
+	
+	private void sendAPDUResponse(APDU apdu, byte[] response)
+	{
+		byte[] buf = apdu.getBuffer();
+		Util.arrayCopy(response, (short)0, buf, ISO7816.OFFSET_CDATA, (short)response.length);
+		apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (short)response.length);
 	}
 }
