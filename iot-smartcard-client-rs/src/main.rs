1220baf081b0b7af0cd7a4c6ca4ebf3d870a223d
Thomas Prévost <7011145@gmail.com>
2022-11-22 21:36:28 +0100

    Add client base skeleton

--- iot-smartcard-client-rs/src/main.rs
+++ iot-smartcard-client-rs/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let smart_card = iot_smartcard_client_rs::smart_card::SmartCard::new().unwrap();
+
+    // Select applet A0 00 00 00 62 03 01 0C 06 01 02
+    let select_applet_apdu_command = apdu::command::select_file(0x04, 0x00, vec![0xA0, 0x00, 0x00, 0x00, 0x62, 0x03, 0x01, 0x0C, 0x06, 0x01, 0x02]);
+    let select_applet_apdu_response = smart_card.send_apdu_command(select_applet_apdu_command.into()).unwrap();
+    assert_eq!((0x90, 0x00), select_applet_apdu_response.trailer); // OK
+
+    // Send hello command
+    let hello_apdu_command_check_size = apdu::Command::new(0x00, 0x10, 0x00, 0x00);
+    let hello_apdu_response_check_size = smart_card.send_apdu_command(hello_apdu_command_check_size).unwrap();
+    assert_eq!(0x6C, hello_apdu_response_check_size.trailer.0); // Wrong length
+
+    let correct_hello_apdu_response_expected_length = hello_apdu_response_check_size.trailer.1;
+    let hello_apdu_command = apdu::Command::new_with_le(0x00, 0x10, 0x00, 0x00, correct_hello_apdu_response_expected_length as u16);
+    let hello_apdu_response = smart_card.send_apdu_command(hello_apdu_command).unwrap();
+    assert_eq!((0x90, 0x00), hello_apdu_response.trailer); // OK
+    assert_eq!(b"Hello robert", hello_apdu_response.payload.as_slice());
 }
---
+++ iot-smartcard-client-rs/src/smart_card.rs
@@ -0,0 +1,56 @@
+use pcsc::{Card, Context, MAX_BUFFER_SIZE, Protocols, Scope, ShareMode};
+use thiserror::Error;
+
+#[allow(dead_code)]
+pub struct SmartCard {
+    pcsc_card: Card,
+}
+
+#[allow(dead_code)]
+impl SmartCard {
+    // We assume here there is only 1 smartcard reader connected to the computer.
+    pub fn new() -> Result<SmartCard, anyhow::Error> {
+        // Establish a PC/SC context.
+        let ctx = Context::establish(Scope::User)
+            .map_err(|err| anyhow::anyhow!("Failed to establish context: {}", err))?;
+
+        // List available readers.
+        let mut readers_buf = [0; 2048];
+        let mut readers = ctx.list_readers(&mut readers_buf)
+            .map_err(|err| anyhow::anyhow!("Failed to list readers: {}", err))?;
+
+        // Use the first reader.
+        let reader = readers.next()
+            .ok_or_else(|| anyhow::anyhow!("No readers are connected."))?;
+
+        let card = ctx.connect(reader, ShareMode::Shared, Protocols::ANY)
+            .map_err(|err| anyhow::anyhow!("Failed to connect to card: {}", err))?;
+
+        Ok(Self {
+            pcsc_card: card
+        })
+    }
+
+    pub fn send_apdu_command(&self, apdu_command: apdu::Command) -> Result<apdu::Response, anyhow::Error> {
+        let mut rapdu_buf = [0; MAX_BUFFER_SIZE];
+
+        let rapdu = self.pcsc_card.transmit(<apdu::Command as Into<Vec<u8>>>::into(apdu_command).as_slice(), &mut rapdu_buf)
+            .map_err(|err| anyhow::anyhow!("Failed to transmit APDU command to card: {}", err))?;
+
+        Ok(rapdu.to_vec().into())
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum SmartCardError {
+    #[error("Failed to establish context: {0}")]
+    FailedToEstablishContext(String),
+    #[error("Failed to list readers: {0}")]
+    FailedToListReaders(String),
+    #[error("No readers are connected.")]
+    NoReadersConnected,
+    #[error("Failed to connect to card: {0}")]
+    FailedToConnectToCard(String),
+    #[error("Failed to transmit APDU command to card: {0}")]
+    FailedToTransmitApduCommandToCard(String),
+}
